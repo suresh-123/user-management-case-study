0809e3d6ac752d31690ce189da7e279396ddf64d
Suresh M <suresh.61060509@ltimindtree.com>
2024-05-27 07:08:11 +0530

    Added test cases

--- backend/tests/userRoutes.test.ts
+++ backend/tests/userRoutes.test.ts
@@ -1,7 +1,21 @@
 import request from "supertest";
-import app from "../src/app"; 
+import app from "../src/app";
 
 describe("User API endpoints", () => {
+  it("should return validation error when input is empty", async () => {
+    const response = await request(app).post("/api/v1/users").send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      success: false,
+      message: [
+        "First name is a required field",
+        "Last name is a required field",
+        "Email is a required field",
+      ],
+    });
+  });
+
   it("should create a new user", async () => {
     const userData = {
       firstName: "Michael",
@@ -19,6 +33,26 @@ describe("User API endpoints", () => {
     expect(response.body.data.user.email).toBe(userData.email);
   });
 
+  it("should return duplicate email error when same email is used", async () => {
+    const newUser = {
+      firstName: "Michael",
+      lastName: "Leo",
+      email: "Michael.leo@dummy.com",
+    };
+
+    // Create the user the first time
+    await request(app).post("/api/v1/users").send(newUser);
+
+    // Try to create the user a second time
+    const response = await request(app).post("/api/v1/users").send(newUser);
+
+    expect(response.status).toBe(409);
+    expect(response.body).toEqual({
+      success: false,
+      message: "Email address is already registered.",
+    });
+  });
+
   it("should get a paginated user list", async () => {
     const response = await request(app).get("/api/v1/users?page=1&per_page=10");
 
@@ -30,5 +64,3 @@ describe("User API endpoints", () => {
     expect(response.body.data.totalCount).toBeDefined();
   });
 });
-
-
